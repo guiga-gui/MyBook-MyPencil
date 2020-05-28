a1ca92e39135fb320e3cfc3f4974bad2ea8b8988
Bernardo Gualberto Silva <57423766+Bernardo01001@users.noreply.github.com>
2020-05-28 11:06:50 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,18 @@
+function salvar() { 
+    var txt = document.querySelector('input#txt')
+    var salvar = document.querySelector('input#salvar')
+    var txtn = arq
+    var arq = window.prompt('Qual nome do arquivo')
+    var comfirmacao = window.confirm('Voce que mostrar seu novo arquivo')
+    if (comfirmacao === true){
+        var show = document.querySelector('div.arquivos')
+        show.innerHTML = txt.value
+    var adicionar = documento.push(arq)
+    } 
+    function mostrar() {
+        var esc = window.prompt('Qual arquivo voce quer ver')
+        var area = document.querySelector('div.arquivos')
+        area.innerHTML = esc.value
+    }
+    
+}
